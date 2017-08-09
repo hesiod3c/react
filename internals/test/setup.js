4288bd1d45500e61bb3db6cb6ee0acfbe4eab282
Hemerson Vianna <hemerson.lourenco@gmail.com>
2017-08-08 23:15:45 -0300

    feature(tests): add setup

---
+++ internals/test/setup.js
@@ -0,0 +1,30 @@
+require('babel-register')();
+
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const exposedProperties = ['window', 'navigator', 'document'];
+const { document } = (new JSDOM('')).window;
+
+global.document = document;
+global.window = document.defaultView;
+global.window.localStorage = global.window.sessionStorage = {
+  getItem(key) {
+    return this[key];
+  },
+  setItem(key, value) {
+    this[key] = value;
+  },
+  removeItem(key) {
+    delete this[key];
+  },
+};
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js',
+};
